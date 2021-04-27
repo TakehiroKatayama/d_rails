ce7b39b4ffca1e98ea4ca6ae2108e09cd33e71a8
TakehiroKatayama <t.katayama.info@gmail.com>
2021-04-27 02:55:40 +0000

    lesson7

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b268633dbdf2968647ceefb36b8212972d993e82d8ca42e18ad5f886769027952c315353093a978e8e21160b084ee08de144b23e3a2dcdd3acc90725c5498939'

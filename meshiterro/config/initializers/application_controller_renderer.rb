ea1367cf4eb8e770ffe435e2228962271429b51a
TakehiroKatayama <t.katayama.info@gmail.com>
2021-04-29 14:03:37 +0000

    lesson3-2

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ccc117b30bf7073742ba63a88ebf330f99f79b2adda1105b46602a8374afaadbac1d6e9f0bccca41bacf76faadf83b8abe10219a5b74cf02bded9f4a33e1d6cd'
--- meshiterro/config/routes.rb
+++ meshiterro/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   #get 'homes/top' コントローラー作成と同時に作成された。
   root to: 'homes#top'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
 
 end
